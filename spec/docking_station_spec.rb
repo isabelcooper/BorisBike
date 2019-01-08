48a6be61db70ae34ff689d833baa8e249f0981b9
isabelcooper <isabel@triptease.com>
2019-01-08 16:00:30 +0000

    refactored bike release test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,14 +7,14 @@ describe DockingStation do
 #  end
   it { is_expected.to respond_to :release_bike}
 
-  it 'should create Bike when release bike is run and check working' do
-    instance = DockingStation.new
-    expect(instance.release_bike.working?).to eq true
-  end
+  # it 'should create Bike when release bike is run and check working' do
+  #   instance = DockingStation.new
+  #   expect(instance.release_bike.working?).to eq true
+  # end
 
-#   it 'releases working bikes' do
-#     bike = subject.release_bike
-#     expect(bike).to be_working
-#   end
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 
 end
