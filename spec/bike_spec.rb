69b44434381f96f89bd59932607adb9db7a825eb
Paul <paul.ayling@bglgroup.co.uk>
2019-01-09 18:17:34 +0000

    Bike check works but prints a message

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,20 +1,17 @@
 require 'bike'
 
 describe Bike do
+
+before (:each) do
+  @bike = Bike.new
+end
+
   it { is_expected.to respond_to :working? }
 
-#context 'bla' do
-#  let (:check_bike_working) {"No"}
-  it 'when user inputs yes, answer is received.' do
-    bike = Bike.new
-    allow($stdin).to receive(:gets).and_return("No")
-    answer = $stdin.gets
-    expect(answer).to eq("No")
-  end
-
   it 'when user inputs yes, bike is no longer working' do
-    bike = Bike.new
-    bike.stub(:gets)
+    allow(@bike).to receive(:gets).and_return("No")
+    @bike.check_bike_working
+    expect(@bike.working?).to eq false
   end
 
 #  end
