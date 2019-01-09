6c3cd7c122182f73bf8dc4a5286a6acccb5259a5
Paul <paul.ayling@bglgroup.co.uk>
2019-01-09 15:21:13 +0000

    Refactored and removed old tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,5 +3,3 @@ require 'bike'
 describe Bike do
   it { is_expected.to respond_to :working? }
 end
-
-puts "Hello"
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,31 +2,20 @@ require 'docking_station'
 
 describe DockingStation do
 
-#  it 'should respond to release_bike when called' do
-#    expect(subject).to respond_to :release_bike
-#  end
-  it { is_expected.to respond_to :release_bike }
-
-  # it 'should create Bike when release bike is run and check working' do
-  #   instance = DockingStation.new
-  #   expect(instance.release_bike.working?).to eq true
-  # end
-
-  it 'releases working bikes' do
-    station = (DockingStation.new)
-    station.dock(Bike.new)
-    bike = station.release_bike
-    expect(bike).to be_working
-  end
-
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it { is_expected.to respond_to :bikes }
+# Dock Test
 
   it 'docks the bike' do
     station = DockingStation.new
     station.dock(bike = Bike.new)
     expect(station.bikes).to include(bike)
+  end
+
+# Release_bike tests
+  it 'releases working bikes' do
+    station = (DockingStation.new)
+    station.dock(Bike.new)
+    bike = station.release_bike
+    expect(bike).to be_working
   end
 
   it 'returns an error if no bikes in the docking station' do
@@ -39,5 +28,5 @@ describe DockingStation do
     station.dock(bike = Bike.new)
     expect(station.release_bike).to eq bike
   end
-  
+
 end
